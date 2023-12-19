b06cb7d75e2033da452db045e9b7c613ebf94598
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-19 13:03:45 -0700

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'black',  'calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'black',  'ferocious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 10, 'F', 'brown',  'very sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 200, 'M', 'black',  'hardcore af', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 14, 'F', 'polar',  'nice', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 21, 'M', 'polar',  'grumpy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 4, 'F', 'brown',  'crazy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 41, 'F', 'brown',  'crazy', 1);
